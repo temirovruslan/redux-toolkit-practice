56e7a63a6b87f4690a5075714eb422b593874fbc
Temirov Ruslan <ruslan.temirov1995@gmail.com>
2023-03-08 11:24:31 +0300

    added-async-fun-in-slice

--- src/App.jsx
+++ src/App.jsx
@@ -1,121 +1,87 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import clsx from "clsx";
 import InputField from "./components/InputField";
 import TodoList from "./components/TodoList";
-import { useDispatch } from "react-redux";
-import { addTodo, removeTodo } from "./store/todoSlice";
-import { Routes, Route } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { addTodo, removeTodo, fetchTodos, addNewTodo } from "./store/todoSlice";
+
 function App() {
 	const [text, setText] = useState("");
-
+	const { status, error } = useSelector((state) => state.todos);
+	// console.log(status, error);
 	// dispatch создает одну функцию которая зрабатывает после какогото события
 	const dispatch = useDispatch();
 
 	const addTask = () => {
-		dispatch(addTodo({ text }));
-		setText("");
-	};
-
-	// const toggleHandleComplited = (todoId) => {
-	// setTodos(
-	// 	todos.map((todo) => {
-	//find coming id in the list
-	// 		if (todo.id !== todoId) return todo;
-	//if we found id i  return ... todo and change value of todo.comlited
-	// 		return { ...todo, completed: !todo.completed };
-	// 	})
-	// );
-	// };
-
-	// const deleteTodos = (todoId) => {
-	// получаем todoId
-	//  в setTodos оставляем только те котоый id не равен todoId
-	// setTodos(todos.filter((todo) => todo.id !== todoId));
-	// };
-
-	const [lists, setLists] = useState([]);
-	const [text2, setText2] = useState("");
-	console.log(lists);
-
-	const addText = () => {
-		if (text2.trim().length) {
-			setLists([
-				...lists,
-				{
-					id: new Date().toISOString(),
-					completed: false,
-					text2: text2,
-				},
-			]);
-			setText2("");
+		if (text.trim().length) {
+			dispatch(addNewTodo(text));
+			setText("");
 		}
 	};
 
-	const deleteList = (listId) => {
-		setLists(lists.filter((list) => list.id !== listId));
-	};
-
-	const toggle = (listId) => {
-		setLists(
-			lists.map((list) => {
-				if (list.id !== listId) return list;
-
-				return { ...list, completed: !list.completed };
-			})
-		);
-	};
+	// dispatch(fetchTodos()) это вызовет нашу функцию в todoSlice - fetchTodos
+	useEffect(() => {
+		dispatch(fetchTodos());
+	}, [dispatch]);
 
 	return (
-		<div className="App">
-			{/* <div className="flex flex-col  w-64">
+		<div className="App flex">
+			<div className="flex flex-col  w-64">
 				<InputField
 					text={text}
 					handleInput={setText}
 					handleSubmit={addTask}
 				/>
-
+				{status === "loading" && <p>loading...</p>}
+				{error && <p>{error}</p>}
 				<TodoList />
-			</div> */}
-
-			<div className="w-[500px] h-[500px] border bg-slate-200">
-				<label className="flex flex-col items-center">
-					<input
-						onChange={(e) => setText2(e.target.value)}
-						type="text"
-						value={text2}
-						className="w-3/5"
-					/>
-					<button
-						onClick={addText}
-						className="border bg-slate-500 px-2 py-2 rounded-md hover:bg-orange-500"
-					>
-						Submit
-					</button>
-				</label>
-				<ul>
-					{lists.map((list) => {
-						return (
-							<li key={list.id}>
-								<input
-									onChange={() => toggle(list.id)}
-									type="checkbox"
-									checked={list.completed}
-								/>
-								<span>{list.text2}</span>
-								<span
-									className="cursor-pointer"
-									onClick={() => deleteList(list.id)}
-								>
-									&times;
-								</span>
-							</li>
-						);
-					})}
-				</ul>
 			</div>
 		</div>
 	);
 }
 
 export default App;
+
+// const toggleHandleComplited = (todoId) => {
+// setTodos(
+// 	todos.map((todo) => {
+//find coming id in the list
+// 		if (todo.id !== todoId) return todo;
+//if we found id i  return ... todo and change value of todo.comlited
+// 		return { ...todo, completed: !todo.completed };
+// 	})
+// );
+// };
+
+// const deleteTodos = (todoId) => {
+// получаем todoId
+//  в setTodos оставляем только те котоый id не равен todoId
+// setTodos(todos.filter((todo) => todo.id !== todoId));
+// };
+// const addText = () => {
+// 	if (text2.trim().length) {
+// 		setLists([
+// 			...lists,
+// 			{
+// 				id: new Date().toISOString(),
+// 				completed: false,
+// 				text2: text2,
+// 			},
+// 		]);
+// 		setText2("");
+// 	}
+// };
+
+// const deleteList = (listId) => {
+// 	setLists(lists.filter((list) => list.id !== listId));
+// };
+
+// const toggle = (listId) => {
+// 	setLists(
+// 		lists.map((list) => {
+// 			if (list.id !== listId) return list;
+
+// 			return { ...list, completed: !list.completed };
+// 		})
+// 	);
+// };
